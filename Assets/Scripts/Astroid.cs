bf7bccd33d4f33d35dac552622a95ca6b62aeb50
Moris rafol <moris.rafol@gmail.com>
2018-01-26 04:44:57 +0200

    meaw #4
    complened astroid movement
    started working on the astroid spawners

--- Assets/Scripts/Astroid.cs
+++ Assets/Scripts/Astroid.cs
@@ -6,11 +6,20 @@ public class Astroid :   MonoBehaviour
     ,Events.Groups.Resetable.IAll_Group_Events
 {
     [SerializeField] private GameObject instance;
+    [SerializeField] private AstroidMovement movementComponent;
+
+    public GameObject GameObjectInstance
+    {
+        get { return instance; }
+    }
+    public AstroidMovement MovementComponent
+    {
+        get { return movementComponent; }
+    }
 
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
