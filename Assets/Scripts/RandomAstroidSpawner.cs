bf8176ff14354c37ee54f30ffd092c3ea9e5ca1b
Moris rafol <moris.rafol@gmail.com>
2018-01-26 10:07:25 +0200

    astroid spawners
    -workig line spawner
    -working random spawner

---
+++ Assets/Scripts/RandomAstroidSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class RandomAstroidSpawner  : AstroidSpawner
+{
+    private float timeSample;
+
+    public override void StartSpawn()
+    {
+        base.StartSpawn();
+        timeSample = Time.deltaTime;
+
+    }
+
+    protected override bool Spawn()
+    {
+        if(!base.Spawn())return false;
+        if (Time.time - timeSample > GetRandomDelayBetweenSecondsInRange())
+        {
+            timeSample = Time.time;
+            var astroid =  GetReadyAstroid();
+            astroid.Instance.transform.position = GetRandomPosInRange();
+            astroid.Instance.SetActive(true);
+            astroid.InitlialInitliaze( Vector3.left, GetRandomSpeedInRange()); //todo get random Direction
+
+        }
+        return true;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if(Input.GetKeyDown(KeyCode.X))
+            StartSpawn();
+    }
+}
--- Assets/Scripts/StraightAstroidSpawner.cs
+++ Assets/Scripts/StraightAstroidSpawner.cs
@@ -1,34 +1,48 @@
 ﻿using UnityEngine;
 
-public class StraightAstroidSpawner: AstroidSpawner
+
+public class StraightAstroidSpawner : AstroidSpawner
 {
     private float timeSample;
+    private Vector3 instantiatePos;
+
 
     public override void StartSpawn()
     {
         base.StartSpawn();
+        if(isSpawning)return;
         timeSample = Time.deltaTime;
+        instantiatePos = GetRandomPosInRange();
 
     }
 
     protected override bool Spawn()
     {
-        if(!base.Spawn())return false;
+        if (!base.Spawn()) return false;
         if (Time.time - timeSample > GetRandomDelayBetweenSecondsInRange())
         {
-            var astroid =  GetReadyAstroid();
-            astroid.Instance.transform.position = GetRandomPosInRange();
+            timeSample = Time.time;
+            var astroid = GetReadyAstroid();
+            astroid.Instance.transform.position = instantiatePos;
             astroid.Instance.SetActive(true);
-            astroid.InitlialInitliaze( Vector3.left, GetRandomSpeedInRange()); //todo get random Direction
+            astroid.InitlialInitliaze(Vector3.left, GetRandomSpeedInRange()); //todo get random Direction
 
         }
         return true;
     }
 
+    protected override void Start()
+    {
+        base.Start();
+        instantiatePos = GetRandomPosInRange();
+    }
+
     protected override void Update()
     {
         base.Update();
-        if(Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.X))
             StartSpawn();
     }
+
+
 }
