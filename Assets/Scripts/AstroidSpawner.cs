e558863b72b591850803fb28985b7b192d451468
Moris rafol <moris.rafol@gmail.com>
2018-01-26 19:07:27 +0200

    implemented wave class

--- Assets/Scripts/AstroidSpawner.cs
+++ Assets/Scripts/AstroidSpawner.cs
@@ -8,10 +8,67 @@ using Random = UnityEngine.Random;
 
 
 [Serializable]
-public class Wave
+public class Wave : MonoBehaviour
 {
-  // List<AstroidSpawner>
+    //  [SerializeField] private <AstroidSpawner> astroidSpawner;
+    [SerializeField] private List<WaveSettings> _waves;
 
+    private bool isRunning;
+    public void StartWave()
+    {
+       // StopCoroutine("WaveStartCorotiene");
+      if(!isRunning)
+            StartCoroutine("WaveStartCorotiene");
+    }
+    IEnumerator WaveStartCorotiene()
+    {
+        lock (gameObject)
+        {
+            if (isRunning) yield break;
+            isRunning = true;
+            for (int i = 0; i < _waves.Count; i++)
+            {
+                if (false /*todo: is paused*/) yield return StartCoroutine(WaitUntilEnabled());
+
+                _waves[i].Spawner.StartSpawn();
+                yield return new WaitForSeconds(_waves[i].Delay);
+            }
+            isRunning = false;
+        }
+    }
+    IEnumerator WaitUntilEnabled()
+    {
+        while (true) //todo: is gamePaused
+        {
+
+        }
+
+    }
+
+    public bool IsRunning { get { return isRunning;} }
+
+
+}
+
+public class WaveSettings
+{
+    [SerializeField] private float delay;
+    [SerializeField] private AstroidSpawner spawner;
+
+    public AstroidSpawner Spawner
+    {
+        get
+        {
+            return spawner;
+        }
+
+
+    }
+    public float Delay
+    {
+        get { return delay; }
+
+    }
 }
 
 
