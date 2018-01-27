b391235f82a4a01938bc6cfad26c4d4c60a450b0
SilentGodot <morohana17@Gmail.com>
2018-01-27 14:05:20 +0200

    -Add opening movie

---
+++ Assets/Scripts/MoviePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoviePlayer : MonoBehaviour {
+
+    [SerializeField] MovieTexture movie;
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<RawImage>().texture = movie;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = movie.audioClip;
+
+        movie.Play();
+        audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
